23295eef62cd012f060831619b267dccf3208fad
Jesus Reyes <luisdejesus200122@gmail.com>
2022-03-17 17:16:20 -0600

    Singleton como Gestor de Estados

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:singleton_app/pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Singleton App',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => const HomePage(),
+      },
+    );
+  }
+}
